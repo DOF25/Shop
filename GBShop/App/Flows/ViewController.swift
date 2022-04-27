681ef5f75380adf0590ec38849278d98385d6077
Данила Крылов <danila.krylov.1980@mail.ru>
2022-04-27 18:55:42 +0300

    Test push

---
+++ GBShop/App/Flows/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Крылов Данила  on 27.04.2022.
+//
+
+import UIKit
+
+final class TestController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
